ba72f725d70116bdb0e3f4f685ff40f44f874700
mharis99 <mharis99@gmail.com>
2025-08-03 20:33:50 +0200

    initialisation

--- frontend/remix.config.js
+++ frontend/remix.config.js
@@ -1,5 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  appDirectory: "app",
   serverBuildTarget: "vercel",
+  server: "./server.js", // This tells Remix to use custom server (which will be Vercel’s runtime)
   ignoredRouteFiles: ["**/.*"]
 };
