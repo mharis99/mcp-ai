8f32bc27e9036e4fda29b41c8e423487aa2fe78a
mharis99 <mharis99@gmail.com>
2025-08-03 20:38:45 +0200

    fix dependencies

--- frontend/remix.config.js
+++ frontend/remix.config.js
@@ -1,7 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   appDirectory: "app",
-  serverBuildTarget: "vercel",
-  server: "./server.js", // This tells Remix to use custom server (which will be Vercel’s runtime)
   ignoredRouteFiles: ["**/.*"]
 };
